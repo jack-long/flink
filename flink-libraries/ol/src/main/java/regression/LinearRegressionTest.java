1b4f9f17bfeafbb2bcee79e87d593505e5e584d2
Jack <>
2017-12-03 22:58:27 +0100

    Customize data source with addSource method and timestamp and watermarks.
    Stream generated from file is in the same order of the lines.
    timestamp and watermark method is added, though not in use.

--- flink-libraries/ol/src/main/java/regression/LinearRegressionTest.java
+++ flink-libraries/ol/src/main/java/regression/LinearRegressionTest.java
@@ -9,11 +9,15 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
-import sun.jvm.hotspot.oops.Array;
+import org.apache.flink.streaming.api.watermark.Watermark;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 import static org.apache.flink.core.fs.FileSystem.WriteMode.OVERWRITE;
 
@@ -28,22 +32,28 @@ public class LinearRegressionTest {
         // operate in Event-time
         // env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        // create a checkpoint every 5 seconds
-        // env.enableCheckpointing(5000);
-        // try to restart 60 times with 10 seconds delay (10 Minutes)
-        // env.setRestartStrategy(RestartStrategies.fixedDelayRestart(60, Time.of(10, TimeUnit.SECONDS)));
-
         // start the data generator
-        DataStream<ArrayList<Double>> dataStream = env.readTextFile(input)
-                .map(new VectorExtractor());
+        // DataStream<ArrayList<Double>> dataStream = env.readTextFile(input)
+        //        .map(new VectorExtractor());
+
+        DataStream<List<Double>> dataStream = env.addSource(new streamFromFile(input))
+                .map(new VectorExtractor()); //.assignTimestampsAndWatermarks(new TimestampExtractor());
+
+        // remove the original timestamp at the fist position.
+        // DataStream<List<Double>> preparedDataStream = dataStream.map(new GetValues());
+
         // dataStream.writeAsText("output", OVERWRITE).setParallelism(1);
 
         // Reference FlinkML & Spark MLlib
         LinearRegression lr = new LinearRegression(1);
 
-        // LinearRegression.fit(input) => Tuple2<input, model>
-        DataStream<Tuple3<ArrayList<Double>, ArrayList<Double>, Double>> inputWithModel = lr.fit(dataStream);
+        // LinearRegression.fit(input) => Tuple3<input, model, error_score>
+        DataStream<Tuple3<List<Double>, List<Double>, Double>> inputWithModel = lr.fit(dataStream);
+
+        // write to file
         inputWithModel.writeAsText(output, OVERWRITE); //.setParallelism(1);
+        // print
+        // inputWithModel.print();
 
         // LinearRegression.predict(Tuple2<input, model>) => prediction
         // DataStream<Tuple2<Double, Double>> predictions = lr.predict(inputWithModel);
@@ -58,32 +68,101 @@ public class LinearRegressionTest {
         env.execute("Linear predictor");
     }
 
-    /**
-     * String => Tuple2<ArrayList<Double>, Double>
-     *
-     * Transform labeled training data to < data, label >
-     */
-    // public static class VectorExtractor implements MapFunction<String, Tuple2<ArrayList<Double>, Double>> {
-    //    @Override
-    //    public Tuple2<ArrayList<Double>, Double> map(String s) throws Exception {
-    //        String[] elements = s.split(",");
-    //        ArrayList<Double> doubleElements = new ArrayList<>(elements.length - 1);
-    //        for (int i = 0; i < elements.length - 1; i++) {
-    //            doubleElements.add(new Double(elements[i]));
-    //        }
-    //        return new Tuple2<>(doubleElements, new Double(elements[elements.length - 1])) ;
-    //    }
-    // }
 
-    public static class VectorExtractor implements MapFunction<String, ArrayList<Double>> {
+    /**
+     * Generate a stream line by line from a file.
+     */
+    public static class streamFromFile implements SourceFunction<String> {
+        boolean isRunning;
+        String inputFile;
+
+        streamFromFile(String inputFile) throws FileNotFoundException {
+            isRunning = true;
+            this.inputFile = inputFile;
+        }
+
+        /**
+         * Starts the source. Implementations can use the {@link SourceContext} emit
+         * elements.
+         *
+         * @param ctx The context to emit elements to and for accessing locks.
+         */
         @Override
-        public ArrayList<Double> map(String s) throws Exception {
+        public void run(SourceContext<String> ctx) throws Exception {
+            FileReader fr;
+            BufferedReader br;
+            fr = new FileReader(inputFile);
+            br = new BufferedReader(fr);
+            String line;
+            while (isRunning) {
+                if ((line = br.readLine()) != null) {
+                    ctx.collect(line);
+                } else {
+                    break;
+                }
+                Thread.sleep(10);
+            }
+            fr.close();
+            fr = null;
+            br.close();
+            br = null;
+        }
+
+        /**
+         * Cancels the source.
+         */
+        @Override
+        public void cancel() {
+            isRunning = false;
+        }
+    }
+
+    /**
+     * Assign tiemstamp and watermarks.
+     * <p>
+     * Get timestamp at the first value.
+     */
+    public static class TimestampExtractor implements AssignerWithPeriodicWatermarks<ArrayList<Double>> {
+        private final long maxOutOfOrderness = 3000; // 3 seconds
+
+        private long currentMaxTimestamp;
+
+        @Override
+        public long extractTimestamp(ArrayList<Double> data, long previousElementTimestamp) {
+            long newTimestamp = data.get(0).longValue();
+            if (newTimestamp > currentMaxTimestamp) {
+                currentMaxTimestamp = newTimestamp;
+            }
+            return newTimestamp;
+        }
+
+        @Override
+        public Watermark getCurrentWatermark() {
+            return new Watermark(currentMaxTimestamp - maxOutOfOrderness);
+        }
+    }
+
+    public static class VectorExtractor implements MapFunction<String, List<Double>> {
+        @Override
+        public List<Double> map(String s) throws Exception {
             String[] elements = s.split(",");
-            ArrayList<Double> doubleElements = new ArrayList<>(elements.length);
+            List<Double> doubleElements = new ArrayList<>(elements.length);
             for (int i = 0; i < elements.length; i++) {
                 doubleElements.add(new Double(elements[i]));
             }
-            return new ArrayList<>(doubleElements) ;
+            return new ArrayList<>(doubleElements);
+        }
+    }
+
+    /**
+     * Get the valid data values.
+     * <p>
+     * Remove original timestamples at the beginning of the List.
+     */
+    public static class GetValues implements MapFunction<List<Double>, List<Double>> {
+        @Override
+        public List<Double> map(List<Double> input) throws Exception {
+            return new ArrayList<>(input.subList(1, input.size()));
         }
     }
 
